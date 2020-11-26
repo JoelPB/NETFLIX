a1aa6aac7785e92927ea397612fda6c821408d05
JoelPB <coffee.program.joel@gmail.com>
2020-11-26 18:52:12 -0300

    Parte 9

---
+++ js/function.js
@@ -0,0 +1,7 @@
+function assistir(){
+    window.open("https://www.youtube.com/watch?v=SvSkxBYuoQY");
+}
+
+function informar(){
+    window.open("https://pt.wikipedia.org/wiki/House_of_Cards_(série_americana)");
+}
